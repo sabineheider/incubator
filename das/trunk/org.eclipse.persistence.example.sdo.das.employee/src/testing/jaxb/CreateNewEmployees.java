1c9405798bb9711f4269b31b48fad4ff5947f681
Doug Clarke <douglas.clarke@oracle.com>
2009-01-08 17:25:13 +0000

    SDO-JPA DAS Incubator (Bug 258057) Updated jars with container-accessor fix and Employee example JAXB tests working

--- das/trunk/org.eclipse.persistence.example.sdo.das.employee/src/testing/jaxb/CreateNewEmployees.java
+++ das/trunk/org.eclipse.persistence.example.sdo.das.employee/src/testing/jaxb/CreateNewEmployees.java
@@ -18,19 +18,23 @@
  ******************************************************************************/
 package testing.jaxb;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
 import javax.xml.bind.*;
 
 import model.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static junit.framework.Assert.*;
 
 public class CreateNewEmployees {
 
 	private static JAXBContext jaxbContext;
 
 	@Test
-	public void simple() throws JAXBException {
+	public void simple() throws Exception {
 		Employee employee = new Employee();
 		employee.setId(1);
 		employee.setFirstName("John");
@@ -47,12 +51,42 @@ public class CreateNewEmployees {
 		address.setCountry("Canada");
 		employee.setAddress(address);
 		
-		employee.addPhoneNumber("Home", "6135551212");
-		employee.addPhoneNumber("Mobile", "6135552121");
+		PhoneNumber p1 = employee.addPhoneNumber("Home", "6135551212");
+		assertSame(employee, p1.getOwner());
+		PhoneNumber p2 = employee.addPhoneNumber("Mobile", "6135552121");
+		assertSame(employee, p2.getOwner());
 
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		marshaller.marshal(employee, System.out);
+		StringWriter writer = new StringWriter();
+		marshaller.marshal(employee, writer);
+		
+		System.out.println("XML::\n" + writer.toString());
+		
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		
+		Employee emp2 = (Employee) unmarshaller.unmarshal(new StringReader(writer.toString()));
+		
+		assertNotNull(emp2);
+		assertNotSame(employee, emp2);
+		assertEquals(employee.getId(), emp2.getId());
+		assertEquals(employee.getFirstName(), emp2.getFirstName());
+		assertEquals(employee.getLastName(), emp2.getLastName());
+		assertEquals(employee.getGender(), emp2.getGender());
+		assertEquals(employee.getSalary(), emp2.getSalary());
+		assertEquals(employee.getVersion(), emp2.getVersion());
+		
+		assertNotNull(emp2.getAddress());
+		assertEquals(employee.getAddress().getId(), emp2.getId());
+		assertEquals(employee.getAddress().getCity(), emp2.getAddress().getCity());
+		assertEquals(employee.getAddress().getStreet(), emp2.getAddress().getStreet());
+		assertEquals(employee.getAddress().getProvince(), emp2.getAddress().getProvince());
+		assertEquals(employee.getAddress().getPostalCode(), emp2.getAddress().getPostalCode());
+		assertEquals(employee.getAddress().getCountry(), emp2.getAddress().getCountry());
+		
+		assertEquals(2, emp2.getPhoneNumbers().size());
+		assertSame(emp2, emp2.getPhoneNumbers().get(0).getOwner());
+		assertSame(emp2, emp2.getPhoneNumbers().get(1).getOwner());
 	}
 
 	@BeforeClass
