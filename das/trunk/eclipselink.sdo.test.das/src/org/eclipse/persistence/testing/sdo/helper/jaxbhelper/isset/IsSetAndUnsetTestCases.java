39676d6579a922e999f3848beaabbe2dcc495252
Blaise Doughan <blaise.doughan@oracle.com>
2009-01-23 21:50:21 +0000

    Added another test case.

--- das/trunk/eclipselink.sdo.test.das/src/org/eclipse/persistence/testing/sdo/helper/jaxbhelper/isset/IsSetAndUnsetTestCases.java
+++ das/trunk/eclipselink.sdo.test.das/src/org/eclipse/persistence/testing/sdo/helper/jaxbhelper/isset/IsSetAndUnsetTestCases.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Copyright (c) 1998, 2008 Oracle. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *    mmacivor - JPA DAS INCUBATOR - Enhancement 258057
  *               http://wiki.eclipse.org/EclipseLink/Development/SDO-JPA
  *
- * This code is being developed under INCUBATION and is not currently included 
- * in the automated EclipseLink build. The API in this code may change, or 
- * may never be included in the product. Please provide feedback through mailing 
+ * This code is being developed under INCUBATION and is not currently included
+ * in the automated EclipseLink build. The API in this code may change, or
+ * may never be included in the product. Please provide feedback through mailing
  * lists or the bug database.
  ******************************************************************************/
 package org.eclipse.persistence.testing.sdo.helper.jaxbhelper.isset;
@@ -32,62 +32,102 @@ import commonj.sdo.Property;
 
 public class IsSetAndUnsetTestCases extends SDOTestCase {
     private static final String XML_SCHEMA = "org/eclipse/persistence/testing/sdo/helper/jaxbhelper/isset/isset.xsd";
-    
+
     private JAXBHelperContext jaxbHelperContext;
-    
+
     public IsSetAndUnsetTestCases(String name) {
         super(name);
     }
-    
+
     public void setUp() {
         IsSetProject project = new IsSetProject();
         XMLContext xmlContext = new XMLContext(project);
         JAXBContext jaxbContext = new JAXBContext(xmlContext);
         jaxbHelperContext = new JAXBHelperContext(jaxbContext);
-        
+
         InputStream xsd = Thread.currentThread().getContextClassLoader().getResourceAsStream(XML_SCHEMA);
         jaxbHelperContext.getXSDHelper().define(xsd, null);
     }
-    
-    public void test1() throws IOException {
-    	DataObject root1 = jaxbHelperContext.getDataFactory().create("urn:isset", "root");
-    	
-    	DataObject child1 = jaxbHelperContext.getDataFactory().create("urn:isset", "child");
-    	DataObject child2 = jaxbHelperContext.getDataFactory().create("urn:isset", "child");
 
-    	Property childProp = root1.getType().getProperty("child");
-    	Property childManyProp = root1.getType().getProperty("child-many");
-    	
-    	assertFalse(root1.isSet(childProp));
-    	assertFalse(root1.isSet(childManyProp));
-    	
-    	ArrayList list = new ArrayList();
-    	root1.setList(childManyProp, list);
-    	
-    	//empty list means isSet = false
-    	assertFalse(root1.isSet(childManyProp));
-    	root1.getList(childManyProp).add(child1);
-    	assertTrue(root1.isSet(childManyProp));
-    	
-    	root1.getList(childManyProp).add(child2);
-    	root1.unset(childManyProp);
-    	
-    	assertTrue(root1.getList(childManyProp).size() == 0);
-    	assertFalse(root1.isSet(childManyProp));
-    	
-    	root1.set(childProp, child2);
-    	assertTrue(root1.isSet(childProp));
-    	root1.set(childProp, null);
-    	assertFalse(root1.isSet(childProp));
-    	root1.set(childProp, child2);
-    	root1.unset(childProp);
-    	assertFalse(root1.isSet(childProp));
-    	assertTrue(root1.get(childProp) == null);
-    	
+    public void test1() throws IOException {
+        DataObject root1 = jaxbHelperContext.getDataFactory().create("urn:isset", "root");
+
+        DataObject child1 = jaxbHelperContext.getDataFactory().create("urn:isset", "child");
+        DataObject child2 = jaxbHelperContext.getDataFactory().create("urn:isset", "child");
+
+        Property childProp = root1.getType().getProperty("child");
+        Property childManyProp = root1.getType().getProperty("child-many");
+
+        assertFalse(root1.isSet(childProp));
+        assertFalse(root1.isSet(childManyProp));
+
+        ArrayList list = new ArrayList();
+        root1.setList(childManyProp, list);
+
+        //empty list means isSet = false
+        assertFalse(root1.isSet(childManyProp));
+        root1.getList(childManyProp).add(child1);
+        assertTrue(root1.isSet(childManyProp));
+
+        root1.getList(childManyProp).add(child2);
+        root1.unset(childManyProp);
+
+        assertTrue(root1.getList(childManyProp).size() == 0);
+        assertFalse(root1.isSet(childManyProp));
+
+        root1.set(childProp, child2);
+        assertTrue(root1.isSet(childProp));
+        root1.set(childProp, null);
+        assertFalse(root1.isSet(childProp));
+        root1.set(childProp, child2);
+        root1.unset(childProp);
+        assertFalse(root1.isSet(childProp));
+        assertTrue(root1.get(childProp) == null);
+
     }
-    
+
+    public void test2() throws IOException {
+        Root rootPOJO = new Root();
+        DataObject root1 = jaxbHelperContext.wrap(rootPOJO);
+
+        Child child1POJO = new Child();
+        DataObject child1 = jaxbHelperContext.wrap(child1POJO);
+
+        Child child2POJO = new Child();
+        DataObject child2 = jaxbHelperContext.wrap(child2POJO);
+
+        Property childProp = root1.getType().getProperty("child");
+        Property childManyProp = root1.getType().getProperty("child-many");
+
+        assertFalse(root1.isSet(childProp));
+        assertFalse(root1.isSet(childManyProp));
+
+        ArrayList list = new ArrayList();
+        root1.setList(childManyProp, list);
+
+        //empty list means isSet = false
+        assertFalse(root1.isSet(childManyProp));
+        root1.getList(childManyProp).add(child1);
+        assertTrue(root1.isSet(childManyProp));
+
+        root1.getList(childManyProp).add(child2);
+        root1.unset(childManyProp);
+
+        assertTrue(root1.getList(childManyProp).size() == 0);
+        assertFalse(root1.isSet(childManyProp));
+
+        root1.set(childProp, child2);
+        assertTrue(root1.isSet(childProp));
+        root1.set(childProp, null);
+        assertFalse(root1.isSet(childProp));
+        root1.set(childProp, child2);
+        root1.unset(childProp);
+        assertFalse(root1.isSet(childProp));
+        assertTrue(root1.get(childProp) == null);
+
+    }
+
     public void tearDown() {
-    }    
-    
+    }
 
 }
