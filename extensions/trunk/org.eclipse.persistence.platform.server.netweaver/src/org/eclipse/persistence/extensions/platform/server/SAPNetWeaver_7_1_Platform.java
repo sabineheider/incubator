755c3bcc4b451997cc81ace5f063fcc5d476dc3c
Tom Ware <tom.ware@oracle.com>
2009-09-30 12:27:02 +0000

    update copyright

--- extensions/trunk/org.eclipse.persistence.platform.server.netweaver/src/org/eclipse/persistence/extensions/platform/server/SAPNetWeaver_7_1_Platform.java
+++ extensions/trunk/org.eclipse.persistence.platform.server.netweaver/src/org/eclipse/persistence/extensions/platform/server/SAPNetWeaver_7_1_Platform.java
@@ -1,11 +1,23 @@
-+package org.eclipse.persistence.platform.server.sap;
-+
-+import javax.persistence.spi.PersistenceUnitInfo;
-+
-+import org.eclipse.persistence.internal.helper.JPAClassLoaderHolder;
-+import org.eclipse.persistence.logging.AbstractSessionLog;
-+import org.eclipse.persistence.platform.server.ServerPlatformBase;
-+import org.eclipse.persistence.sessions.DatabaseSession;
+/*******************************************************************************
+ * Copyright (c) 1998, 2009 SAP. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     SAP - initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.platform.server.sap;
+
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import org.eclipse.persistence.internal.helper.JPAClassLoaderHolder;
+import org.eclipse.persistence.logging.AbstractSessionLog;
+import org.eclipse.persistence.platform.server.ServerPlatformBase;
+import org.eclipse.persistence.sessions.DatabaseSession;
 import org.eclipse.persistence.transaction.sap.SAPNetWeaverTransactionController;
 
 public class SAPNetWeaver_7_1_Platform extends ServerPlatformBase {
