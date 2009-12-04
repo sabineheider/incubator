769bec22a743afff23cbaa701e315bfae9f27a42
Doug Clarke <douglas.clarke@oracle.com>
2009-12-04 19:48:17 +0000

    Bug 288307 - Extension Incubator: FetchPlan - Fixed FetchPlanAssert to properly handle nested relationships. Object[] results not yet supported

--- extensions/trunk/org.eclipse.persistence.example.jpa.fetchplan/test-src/test/fetchplan/FetchPlanAssert.java
+++ extensions/trunk/org.eclipse.persistence.example.jpa.fetchplan/test-src/test/fetchplan/FetchPlanAssert.java
@@ -21,6 +21,7 @@ import org.eclipse.persistence.extension.fetchplan.FetchPlan;
 import org.eclipse.persistence.indirection.IndirectContainer;
 import org.eclipse.persistence.indirection.ValueHolderInterface;
 import org.eclipse.persistence.mappings.DatabaseMapping;
+import org.eclipse.persistence.queries.FetchGroupTracker;
 
 /**
  * Helper class used by test cases to ensure that the expected attributes and
@@ -34,21 +35,53 @@ public class FetchPlanAssert {
     public static void assertFetched(FetchItem fetchItem, Object result) {
         Assert.assertNotNull("Null FetchItem", fetchItem);
         Assert.assertNotNull("Null Result", result);
+        Assert.assertFalse("Object[] results not yet supported", result instanceof Object[]);
 
         DatabaseMapping[] mappings = fetchItem.getMappings();
         Assert.assertNotNull(mappings);
 
         Object current = result;
         for (int index = 0; index < mappings.length; index++) {
+
             Object value = mappings[index].getAttributeValueFromObject(current);
 
             if (value instanceof IndirectContainer) {
                 Assert.assertTrue(((IndirectContainer) value).isInstantiated());
             } else if (value instanceof ValueHolderInterface) {
                 Assert.assertTrue(((ValueHolderInterface) value).isInstantiated());
+                value = ((ValueHolderInterface) value).getValue();
             }
             current = value;
+            
+            if (current == null) {
+                break;
+            }
         }
+    }
+
+    public static Object assertLoaded(DatabaseMapping mapping, Object entity) {
+        Assert.assertEquals("Incorrect class", mapping.getDescriptor().getJavaClass(), entity.getClass());
+        
+        if (entity instanceof Collection<?>) {
+            for (Object o : ((Collection<?>) entity)) {
+                assertLoaded(mapping, o);
+            }
+            return null;
+        }
+
+        Object value = mapping.getAttributeValueFromObject(entity);
+
+        if (mapping.isDatabaseMapping()) {
+            if (entity instanceof FetchGroupTracker) {
+                Assert.assertTrue("Attribute not fetched: " + mapping.getAttributeName(), ((FetchGroupTracker) entity)._persistence_isAttributeFetched(mapping.getAttributeName()));
+            }
+        } else if (value instanceof IndirectContainer) {
+            Assert.assertTrue(((IndirectContainer) value).isInstantiated());
+        } else if (value instanceof ValueHolderInterface) {
+            Assert.assertTrue(((ValueHolderInterface) value).isInstantiated());
+            value = ((ValueHolderInterface) value).getValue();
+        }
+        return value;
     }
 
     public static void assertFetched(FetchPlan fetchPlan, Object result) {
