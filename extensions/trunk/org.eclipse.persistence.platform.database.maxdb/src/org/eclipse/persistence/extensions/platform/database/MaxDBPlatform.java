b4882a7065c9429cdc1f4ab3758ad4ac28c4d699
Andreas Fischbach <andreas.fischbach@sap.com>
2010-06-02 13:14:31 +0000

    expand maxdbplatform to get srg and wdf tests running (not finished yet)

--- extensions/trunk/org.eclipse.persistence.platform.database.maxdb/src/org/eclipse/persistence/extensions/platform/database/MaxDBPlatform.java
+++ extensions/trunk/org.eclipse.persistence.platform.database.maxdb/src/org/eclipse/persistence/extensions/platform/database/MaxDBPlatform.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Markus Karg. All rights reserved.
+ * Copyright (c) 2009,2010 Markus Karg, SAP. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -9,14 +9,17 @@
  *
  * Contributors:
  * Markus Karg - Initial implementation
+ * Andreas Fischbach - get tests running with maxdb
  *
  * This code is being developed under INCUBATION and is not currently included 
  * in the automated EclipseLink build. The API in this code may change, or 
  * may never be included in the product. Please provide feedback through mailing 
  * lists or the bug database.
- ******************************************************************************/  
-package org.eclipse.persistence.extensions.platform.database;
+ ******************************************************************************/
+package org.eclipse.persistence.platform.database;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Blob;
@@ -25,100 +28,144 @@ import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Hashtable;
+import java.util.Vector;
 
 import org.eclipse.persistence.expressions.ExpressionOperator;
-import org.eclipse.persistence.platform.database.DatabasePlatform;
+import org.eclipse.persistence.expressions.ListExpressionOperator;
 import org.eclipse.persistence.internal.databaseaccess.FieldTypeDefinition;
+import org.eclipse.persistence.internal.helper.ClassConstants;
 import org.eclipse.persistence.internal.helper.DatabaseTable;
 import org.eclipse.persistence.queries.ValueReadQuery;
+import org.eclipse.persistence.tools.schemaframework.FieldDefinition;
 
 /**
  * Provides MaxDB specific behaviour.
  * 
- * @author Markus KARG (markus-karg@users.sourceforge.net)
+ * @author Markus KARG (markus@headcrashing.eu)
  */
 @SuppressWarnings("serial")
 public final class MaxDBPlatform extends DatabasePlatform {
-
-    private static final ExpressionOperator createConcatExpressionOperator() {
-        return ExpressionOperator.simpleTwoArgumentFunction(
-                ExpressionOperator.Concat, "CONCAT");
-    }
-
-    private static final ExpressionOperator createEqualOuterJoinOperator() {
-        return ExpressionOperator.simpleRelation(
-                ExpressionOperator.EqualOuterJoin, "(+)=");
-    }
-
-    private static final ExpressionOperator createTrim2ExpressionOperator() {
-        return ExpressionOperator.simpleTwoArgumentFunction(
-                ExpressionOperator.Trim2, "TRIM");
-    }
-
-    /*
-     * TODO Add operators that are not contained in database platform already
-     */
-
+            
     @Override
     protected final Hashtable buildFieldTypes() {
         final Hashtable<Class, FieldTypeDefinition> fieldTypeMapping = new Hashtable<Class, FieldTypeDefinition>();
-        fieldTypeMapping.put(Boolean.class, new FieldTypeDefinition("BOOLEAN",
-                false));
-        fieldTypeMapping.put(Integer.class, new FieldTypeDefinition("INTEGER",
-                false));
-        fieldTypeMapping.put(Long.class, new FieldTypeDefinition("FIXED(20)", false));
-        fieldTypeMapping.put(Float.class, new FieldTypeDefinition("FLOAT",
-                false));
-        fieldTypeMapping.put(Double.class, new FieldTypeDefinition(
-                "DOUBLE PRECISION", false));
-        fieldTypeMapping.put(Short.class, new FieldTypeDefinition("SMALLINT",
-                false));
-        fieldTypeMapping.put(Byte.class, new FieldTypeDefinition("SMALLINT",
-                false));
-        fieldTypeMapping.put(BigInteger.class, new FieldTypeDefinition(
-                "DOUBLE PRECISION", false));
-        fieldTypeMapping.put(BigDecimal.class, new FieldTypeDefinition(
-                "DOUBLE PRECISION", false));
-        fieldTypeMapping.put(Number.class, new FieldTypeDefinition(
-                "DOUBLE PRECISION", false));
-        fieldTypeMapping.put(String.class, new FieldTypeDefinition("CHAR"));
-        fieldTypeMapping.put(Character.class, new FieldTypeDefinition("CHAR"));
-        fieldTypeMapping.put(Byte[].class, new FieldTypeDefinition("LONG BYTE",
-                false));
-        fieldTypeMapping.put(Character[].class, new FieldTypeDefinition(
-                "LONG VARCHAR", false));
-        fieldTypeMapping.put(byte[].class, new FieldTypeDefinition("LONG BYTE",
-                false));
-        fieldTypeMapping.put(char[].class, new FieldTypeDefinition(
-                "LONG VARCHAR", false));
-        fieldTypeMapping.put(Blob.class, new FieldTypeDefinition("LONG BYTE",
-                false));
-        fieldTypeMapping.put(Clob.class, new FieldTypeDefinition(
-                "LONG VARCHAR", false));
-        fieldTypeMapping
-                .put(Date.class, new FieldTypeDefinition("DATE", false));
-        fieldTypeMapping
-                .put(Time.class, new FieldTypeDefinition("TIME", false));
-        fieldTypeMapping.put(Timestamp.class, new FieldTypeDefinition("STAMP",
-                false));
+        fieldTypeMapping.put(Boolean.class, new FieldTypeDefinition("SMALLINT", false)); // TODO boolean
+        fieldTypeMapping.put(Number.class, new FieldTypeDefinition("DOUBLE PRECISION", false));
+        fieldTypeMapping.put(Short.class, new FieldTypeDefinition("SMALLINT", false));
+        fieldTypeMapping.put(Integer.class, new FieldTypeDefinition("INTEGER", false));
+        fieldTypeMapping.put(Long.class, new FieldTypeDefinition("FIXED", 19));
+        fieldTypeMapping.put(Float.class, new FieldTypeDefinition("FLOAT", false));
+        fieldTypeMapping.put(Double.class, new FieldTypeDefinition("DOUBLE PRECISION", false));
+
+        fieldTypeMapping.put(BigInteger.class, new FieldTypeDefinition("FIXED",19));
+        fieldTypeMapping.put(BigDecimal.class, new FieldTypeDefinition("FIXED", 38));
+         
+        fieldTypeMapping.put(Character.class, new FieldTypeDefinition("CHAR", 1, "UNICODE"));
+        fieldTypeMapping.put(Character[].class, new FieldTypeDefinition("VARCHAR", 255, "UNICODE")); 
+        fieldTypeMapping.put(char[].class, new FieldTypeDefinition("VARCHAR", 255, "UNICODE"));
+        fieldTypeMapping.put(String.class, new FieldTypeDefinition("VARCHAR", 255, "UNICODE")); 
+         
+        fieldTypeMapping.put(Byte.class, new FieldTypeDefinition("SMALLINT", false));        
+        fieldTypeMapping.put(Byte[].class, new FieldTypeDefinition("CHAR", 255, "BYTE"));
+        fieldTypeMapping.put(byte[].class, new FieldTypeDefinition("CHAR", 255, "BYTE"));
+        fieldTypeMapping.put(Blob.class, new FieldTypeDefinition("LONG BYTE", false));
+        fieldTypeMapping.put(Clob.class, new FieldTypeDefinition("LONG UNICODE", false));
+
+        fieldTypeMapping.put(Date.class, new FieldTypeDefinition("DATE", false));
+        fieldTypeMapping.put(Time.class, new FieldTypeDefinition("TIME", false));
+        fieldTypeMapping.put(Timestamp.class, new FieldTypeDefinition("TIMESTAMP", false));
         return fieldTypeMapping;
     }
 
-
-/**************
- *  @Override
- *  NOTE CHANGED FUNCTIONALITY SINCE ESSENTIALS, WILL REQUIRE ALTERNATE IMPLEMENTATION FOR ECLIPSELINK
-***************/
-    public final ValueReadQuery buildSelectQueryForNativeSequence(
-            final String sequenceName, final Integer size) {
-        return new ValueReadQuery("SELECT "
-                + this.getQualifiedSequenceName(sequenceName)
-                + ".NEXTVAL FROM DUAL");
+    @Override
+    protected void printFieldTypeSize(Writer writer, FieldDefinition field, FieldTypeDefinition fieldType) throws IOException {        
+        String typeName = fieldType.getName();
+        /* byte[] < 4000 map to CHAR BYTE, longer ones to LONG BYTE */
+        if(typeName.equals("CHAR") && field.getSize() > 4000) {
+            fieldType.setName("LONG");
+            fieldType.setIsSizeRequired(false);
+            fieldType.setIsSizeAllowed(false);
+        }
+        super.printFieldTypeSize(writer, field, fieldType);
+        if (fieldType.getTypesuffix() != null) {
+            writer.append(" " + fieldType.getTypesuffix());
+        }
+    }
+    
+    @Override
+    protected final void initializePlatformOperators() {
+        super.initializePlatformOperators();
+        this.addOperator(MaxDBPlatform.createConcatExpressionOperator());
+        this.addOperator(MaxDBPlatform.createTrim2ExpressionOperator());
+        this.addOperator(MaxDBPlatform.createEqualOuterJoinOperator());
+        this.addOperator(MaxDBPlatform.createToNumberOperator());
+        this.addOperator(MaxDBPlatform.createNullifOperator());
+        this.addOperator(MaxDBPlatform.createCoalesceOperator());
+        this.addNonBindingOperator(MaxDBPlatform.createNullValueOperator());
+    }
+    
+    private static final ExpressionOperator createConcatExpressionOperator() { // not checked
+        return ExpressionOperator.simpleTwoArgumentFunction(ExpressionOperator.Concat, "CONCAT");
     }
 
+    private static final ExpressionOperator createTrim2ExpressionOperator() { // not checked
+        return ExpressionOperator.simpleTwoArgumentFunction(ExpressionOperator.Trim2, "TRIM");
+    }
+
+    private static final ExpressionOperator createEqualOuterJoinOperator() {
+        return ExpressionOperator.simpleRelation(ExpressionOperator.EqualOuterJoin, "(+)=");
+    }
+
+    private static final ExpressionOperator createNullValueOperator() {
+        return ExpressionOperator.simpleTwoArgumentFunction(ExpressionOperator.Nvl, "VALUE");
+    }
+    
+    private static final ExpressionOperator createCoalesceOperator() {
+        ListExpressionOperator operator = (ListExpressionOperator) ExpressionOperator.coalesce();
+        operator.setStartString("VALUE(");
+        operator.setSelector(ExpressionOperator.Coalesce);
+        return operator;
+    }
+    
+    private static final ExpressionOperator createToNumberOperator() {
+        return ExpressionOperator.simpleFunction(ExpressionOperator.ToNumber, "NUM");
+    }
+    
+    private static final ExpressionOperator createNullifOperator() {
+        ExpressionOperator exOperator = new ExpressionOperator();
+        exOperator.setType(ExpressionOperator.FunctionOperator);
+        exOperator.setSelector(ExpressionOperator.NullIf);
+        Vector v = org.eclipse.persistence.internal.helper.NonSynchronizedVector.newInstance(4);
+        v.addElement(" (CASE WHEN ");
+        v.addElement(" = ");
+        v.addElement(" THEN NULL ELSE ");
+        v.addElement(" END) ");
+        exOperator.printsAs(v);
+        exOperator.bePrefix();
+        int[] indices = {0, 1, 0};
+        exOperator.setArgumentIndices(indices);
+        exOperator.setNodeClass(ClassConstants.FunctionExpression_Class);
+        return exOperator;
+    }
+    
     @Override
-    protected final String getCreateTempTableSqlBodyForTable(
-            final DatabaseTable table) {
+    public boolean shouldOptimizeDataConversion() {
+        return true;
+    }
+           
+    protected void addNonBindingOperator(ExpressionOperator operator) {
+        operator.setIsBindingSupported(false);
+        addOperator(operator);
+    }
+    
+    public final ValueReadQuery buildSelectQueryForSequenceObject(final String sequenceName, final Integer size) {
+        return new ValueReadQuery("SELECT " + this.getQualifiedSequenceName(sequenceName) + ".NEXTVAL FROM DUAL");
+    }
+
+    // not checked starts here Andreas <°)))><
+    
+    @Override
+    protected final String getCreateTempTableSqlBodyForTable(final DatabaseTable table) {
         return " LIKE " + table.getQualifiedName();
     }
 
@@ -133,9 +180,7 @@ public final class MaxDBPlatform extends DatabasePlatform {
     }
 
     private final String getQualifiedSequenceName(final String sequenceName) {
-        return this.getTableQualifier().length() == 0 ? sequenceName : this
-                .getTableQualifier()
-                + "." + sequenceName;
+        return this.getTableQualifier().length() == 0 ? sequenceName : this.getTableQualifier() + "." + sequenceName;
     }
 
     @Override
@@ -143,18 +188,11 @@ public final class MaxDBPlatform extends DatabasePlatform {
         return new DatabaseTable("$" + table.getName(), "TEMP");
     }
 
-    @Override
-    protected final void initializePlatformOperators() {
-        super.initializePlatformOperators();
-        this.addOperator(MaxDBPlatform.createConcatExpressionOperator());
-        this.addOperator(MaxDBPlatform.createTrim2ExpressionOperator());
-        this.addOperator(MaxDBPlatform.createEqualOuterJoinOperator());
-    }
+    
 
-/**************
- *  @Override
- *  This will only qualify as an override when isMaxDB appears in DatabasePlatform
-***************/
+    /**************
+     * @Override This will only qualify as an override when isMaxDB appears in DatabasePlatform
+     ***************/
     public final boolean isMaxDB() {
         return true;
     }
